54f103e1cb6164aa6b2257e5f44f983dad5b3a6c
ronysnetDev <dev.ronysnet@gmail.com>
2022-11-27 14:39:13 +0600

    head added

--- pages/Testimonials.tsx
+++ pages/Testimonials.tsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -76,88 +77,102 @@ const Testimonials = () => {
   return (
     <>
       <div className="container">
+        <Head>
+          <title>Testimonial</title>
+          <meta name="description" content="Ifad Autos Tesimonial" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <Top />
         {/* <Nav /> */}
         <Homeslider />
-        <div className="row mt-5">
-          <h1 className="text-center brandColor">TESTIMONIALS</h1>
-          <div className="col-sm-6 offset-sm-3 mb-4">
-            <p className="text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
-              doloribus incidunt perferendis vero. Consequuntur unde accusamus
-              numquam exercitationem
-            </p>
-          </div>
-          {testi.map((st) => {
-            return (
-              <div key={st.id} className="col-sm-4">
-                <div className="shadow mb-5 bg-body rounded position-relative overflow-hidden pb-5">
-                  <h1 className="display-1">
-                    <i className="bi bi-quote"></i>
-                  </h1>
-                  <p className="text-center px-4">{st.detail}</p>
-                  <div className="row d-flex justify-content-center">
+        <main>
+          <div className="row mt-5">
+            <h1 className="text-center brandColor">TESTIMONIALS</h1>
+            <div className="col-sm-6 offset-sm-3 mb-4">
+              <p className="text-center">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam
+                doloribus incidunt perferendis vero. Consequuntur unde accusamus
+                numquam exercitationem
+              </p>
+            </div>
+            {testi.map((st) => {
+              return (
+                <div key={st.id} className="col-sm-4">
+                  <div className="shadow mb-5 bg-body rounded position-relative overflow-hidden pb-5">
+                    <h1 className="display-1">
+                      <i className="bi bi-quote"></i>
+                    </h1>
+                    <p className="text-center px-4">{st.detail}</p>
+                    <div className="row d-flex justify-content-center">
+                      <div
+                        className="d-flex justify-content-center"
+                        style={{
+                          borderRadius: "50%",
+                          overflow: "hidden",
+                          width: "100px",
+                          height: "100px",
+                          zIndex: 333,
+                        }}
+                      >
+                        <Image
+                          objectFit="cover"
+                          width={100}
+                          height={100}
+                          src={`/testimonial/${st.photo}`}
+                          alt="avatar"
+                        />
+                      </div>
+                      <h6
+                        className="text-center mt-2 text-white"
+                        style={avatar}
+                      >
+                        {st.name}
+                      </h6>
+                      <div
+                        className="col-6 d-flex justify-content-evenly text-white"
+                        style={avatar}
+                      >
+                        <Link href={st.fb}>
+                          <i className="bi bi-facebook text-white"></i>
+                        </Link>
+                        <Link href={st.linkedin}>
+                          <i className="bi bi-twitter text-white"></i>
+                        </Link>
+                        <Link href={st.instagram}>
+                          <i className="bi bi-instagram text-white"></i>
+                        </Link>
+                      </div>
+                    </div>
                     <div
-                      className="d-flex justify-content-center"
                       style={{
-                        borderRadius: "50%",
-                        overflow: "hidden",
-                        width: "100px",
-                        height: "100px",
-                        zIndex: 333,
+                        position: "absolute",
+                        width: "100%",
+                        bottom: 0,
+                        left: 0,
+                        zIndex: 0,
                       }}
                     >
-                      <Image
-                        objectFit="cover"
-                        width={100}
-                        height={100}
-                        src={`/testimonial/${st.photo}`}
-                        alt="avatar"
-                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1440 820"
+                      >
+                        <path
+                          fill="#f6821f"
+                          fill-opacity="1"
+                          d="M0,256L48,234.7C96,213,192,171,288,154.7C384,139,480,149,576,176C672,203,768,245,864,234.7C960,224,1056,160,1152,154.7C1248,149,1344,203,1392,229.3L1440,256L1440,820L1392,820C1344,820,1248,820,1152,820C1056,820,960,820,864,820C768,820,672,820,576,820C480,820,384,820,288,820C192,820,96,820,48,820L0,820Z"
+                        ></path>
+                      </svg>
                     </div>
-                    <h6 className="text-center mt-2 text-white" style={avatar}>
-                      {st.name}
-                    </h6>
-                    <div
-                      className="col-6 d-flex justify-content-evenly text-white"
-                      style={avatar}
-                    >
-                      <Link href={st.fb}>
-                        <i className="bi bi-facebook text-white"></i>
-                      </Link>
-                      <Link href={st.linkedin}>
-                        <i className="bi bi-twitter text-white"></i>
-                      </Link>
-                      <Link href={st.instagram}>
-                        <i className="bi bi-instagram text-white"></i>
-                      </Link>
-                    </div>
-                  </div>
-                  <div
-                    style={{
-                      position: "absolute",
-                      width: "100%",
-                      bottom: 0,
-                      left: 0,
-                      zIndex: 0,
-                    }}
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 1440 820"
-                    >
-                      <path
-                        fill="#f6821f"
-                        fill-opacity="1"
-                        d="M0,256L48,234.7C96,213,192,171,288,154.7C384,139,480,149,576,176C672,203,768,245,864,234.7C960,224,1056,160,1152,154.7C1248,149,1344,203,1392,229.3L1440,256L1440,820L1392,820C1344,820,1248,820,1152,820C1056,820,960,820,864,820C768,820,672,820,576,820C480,820,384,820,288,820C192,820,96,820,48,820L0,820Z"
-                      ></path>
-                    </svg>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
+              );
+            })}
+          </div>
+        </main>
       </div>
       <Footer />
     </>

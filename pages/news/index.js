ce4c9658c959f96051dda9540da18f559725c333
rony1980 <rauf.rony@gmail.com>
2022-12-11 18:08:50 +0600

    Update index.js

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,9 @@
-export default getStaticProps = async () => {
+const getStaticProps = async () => {
   const res = await fetch("https://fakestoreapi.com/products/");
   const data = await res.json();
   return {
     props: { rony: data },
   };
 };
+
+export default getStaticProps;

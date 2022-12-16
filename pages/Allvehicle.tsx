d5d3097321e00ad069d56c3ae2845b3eb54bd2ea
ronysnetDev <dev.ronysnet@gmail.com>
2022-12-17 01:11:43 +0600

    product added

--- pages/Allvehicle.tsx
+++ pages/Allvehicle.tsx
@@ -14,42 +14,42 @@ const Allvehicle = () => {
       name: "ICV Distribution",
       detail:
         "A range that offers more power, cabin options, load body options, payload, comfort and savings",
-      image: "v1",
+      image: "bus1.webp",
     },
     {
       id: "2",
       name: "Ashok leyland 2518",
       detail:
         "Move ahed in your trucking business with Eicher Pro 5040 having competitive fuel effiency backed by Volvogroup's eperience",
-      image: "v2",
+      image: "bus2.webp",
     },
     {
       id: "3",
       name: "Ashok leyland 3120 6x2 BS6",
       detail:
         "Modular design, best in class fuel efficiently, superior tyre mileage, higher driving comfort and performance",
-      image: "v3",
+      image: "bus3.webp",
     },
     {
       id: "4",
       name: "Ashok leyland 3120 6x2 BS6",
       detail:
         "Modular design, best in class fuel efficiently, superior tyre mileage, higher driving comfort and performance",
-      image: "v3",
+      image: "bus4.webp",
     },
     {
       id: "5",
       name: "Ashok leyland 3120 6x2 BS6",
       detail:
         "Modular design, best in class fuel efficiently, superior tyre mileage, higher driving comfort and performance",
-      image: "v1",
+      image: "truck1.webp",
     },
     {
       id: "6",
       name: "Ashok leyland 3120 6x2 BS6",
       detail:
         "Modular design, best in class fuel efficiently, superior tyre mileage, higher driving comfort and performance",
-      image: "v2",
+      image: "truck2.webp",
     },
   ];
   return (
@@ -77,7 +77,7 @@ const Allvehicle = () => {
                       <div className="card h-100">
                         <Image
                           className="card-img-top img-fluid"
-                          src={`/discover/${ron.image}.png`}
+                          src={`/product/${ron.image}`}
                           alt="vehicle"
                           width={1920}
                           height={0}

7cc94725d1770cd7d9da1bec5f860db6feff2183
ronysnetDev <dev.ronysnet@gmail.com>
2022-12-25 15:30:59 +0600

    some changes

--- pages/components/Social.tsx
+++ pages/components/Social.tsx
@@ -3,16 +3,13 @@ import React from "react";
 
 const Social = () => {
   const socialImage = [
-    {
-      id: 1,
-      image: "facebook.jpg",
-      code: '<div class="fb-page" data-href="https://www.facebook.com/service.ifad" data-width="300" data-height="" data-hide-cover="false" data-show-facepile="false"></div>',
-    },
-    { id: 2, image: "instagram.jpg", code: "" },
-    { id: 3, image: "linkedin.jpg", code: "" },
-    { id: 4, image: "youtube.jpg", code: "" },
+    { id: 1, image: "facebook.jpg", code:'<div class="fb-page" data-href="https://www.facebook.com/service.ifad" data-width="400" data-height="214" data-hide-cover="false" data-show-facepile="false"></div>' },
+    { id: 2, image: "instagram.jpg",code:"" },
+    { id: 3, image: "linkedin.jpg",code:"" },
+    { id: 4, image: "youtube.jpg",code:"" },
   ];
 
+                
   return (
     <div className="row my-5">
       <h1 className="brandColor text-center fw-bold mb-4">
@@ -22,20 +19,17 @@ const Social = () => {
         <div className="row">
           {socialImage.map((social: any) => {
             return (
-              <>
-                <div key={social.id} className="col-sm-3 mb-sm-0 mb-4">
-                  <div
-                    style={{
-                      backgroundImage: `url(/social/${social.image})`,
-                      width: "300px",
-                      height: "130px",
-                    }}
-                  >
-                    <div
-                      dangerouslySetInnerHTML={{ __html: social.code }}
-                    ></div>
-                  </div>
-                </div>
+               <>
+              <div key={social.id} className="col-sm-3 mb-sm-0 mb-4">
+                
+                
+                <div style={{backgroundImage:`url(/social/${social.image})`, width:"400px", height:"214px"}}>
+                 <div dangerouslySetInnerHTML={{ __html: social.code }}></div> 
+                  </div> 
+                          
+               
+
+              </div>
               </>
             );
           })}

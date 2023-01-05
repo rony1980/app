092da5ac9303c66ce66439134e9aa6da44e99aab
rony1980 <rauf.rony@gmail.com>
2023-01-05 14:45:15 +0600

    Update Investor.jsx
    Dynamic API integrated

--- pages/Investor.jsx
+++ pages/Investor.jsx
@@ -4,7 +4,40 @@ import Head from "next/head";
 import Top from "./components/Top";
 import Homeslider from "./components/Homeslider";
 import Footer from "./components/Footer";
+import Link from "next/link";
 const Investor = () => {
+const [docs, setDocs] = useState([]);
+  useEffect(() => {
+    fetch("http://autosapi.ifadgroup.com:8001/documents")
+      .then((res) => res.json())
+      .then((data) => setDocs(data));
+  }, []);
+const documentButton = docs.map((item,index)=>{
+  return(
+    <>
+      <button className="nav-link text-start" id={item.id} data-bs-toggle="pill" data-bs-target={`#tags-basic_${item.id}`}type="button" role="tab" aria-controls="v-pills-home" aria-selected="true" key={index}>{item.category_name} <i className="bi bi-arrow-right-short"></i></button>      
+    </>
+  )
+})
+
+const document = docs.map((doc)=>{
+  return(
+    <>
+    {doc.all_documents.map(item=>{
+      return(
+        <>
+         <div className="tab-pane fade" id={`tags-basic_${doc.id}`} role="tabpanel" aria-labelledby={item.category_name} tabindex="0">
+          <h4>{doc.category_name}</h4>
+          {item.document_file=!null ? <Link href={item.document_file}>Download</Link> : <></>}
+          </div>
+        </>
+      )
+    })}
+    </>
+  )
+})
+
+ 
   return (
     <>
       <div className="container-fluid">
@@ -17,12 +50,16 @@ const Investor = () => {
         <Homeslider />
       </div>
       <div className="container">
-        <h1 className="brandColor text-center my-5 fw-bold">
-          Investor Information
-        </h1>
+        <h1 className="brandColor text-center my-5 fw-bold">Investor Information</h1>
         <div className="row">
-          <div className="col-sm-4"></div>
-          <div className="col-sm-8">Show details</div>
+          <div className="d-flex align-items-start">
+  <div className="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+    {documentButton}
+  </div>
+  <div className="tab-content" id="v-pills-tabContent">
+    {document }
+  </div>
+</div>
         </div>
       </div>
       <Footer />

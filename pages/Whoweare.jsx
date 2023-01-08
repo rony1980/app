5ff9d0c661ca65a7c33b8f1227a8ad2f27604b84
rony1980 <rauf.rony@gmail.com>
2023-01-08 13:44:30 +0600

    Update Whoweare.jsx

--- pages/Whoweare.jsx
+++ pages/Whoweare.jsx
@@ -7,7 +7,7 @@ import Footer from "./components/Footer";
 const Whoweare = () => {
   const [document, setDocument] = useState([]);
   useEffect(() => {
-    fetch("http://autosapi.ifadgroup.com:8001/content-module/8")
+    fetch("http://autosapi.ifadgroup.com:8001/content-module/23")
       .then((res) => res.json())
       .then((data) => setDocument(data));
   }, []);

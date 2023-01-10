edd7e2278bec2aa0051f0b81da8faae80f8b7ba8
rony1980 <rauf.rony@gmail.com>
2023-01-10 21:10:29 +0600

    Update [id].jsx

--- pages/news/[id].jsx
+++ pages/news/[id].jsx
@@ -8,58 +8,65 @@ import Recentnews from "./Recentnews";
 import Relatednews from "./Reletednews";
 
 export const getStaticPaths = async () => {
-  const res = await fetch("https://fakestoreapi.com/products/");
+  const res = await fetch("http://autosapi.ifadgroup.com:8001/content-module/17");
   const data = await res.json();
+
   const paths = data.map((rony) => {
-    return {
-      params: { id: rony.id.toString() },
-    };
-  });
+    return(
+    rony.content_item.map(item=>{
+    return{
+        params: { id: item.id.toString() },    
+    }
+    })
+    )
+  }).flat();
+  console.log(paths)
   return {
     paths,
-    fallback: false,
-  };
+    fallback:false
+  }
 };
-export async function getStaticProps(context) {
+
+
+export const getStaticProps = async(context)=> {
   const id = context.params.id;
-  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const res = await fetch(`http://implapi.ifadgroup.com:8001/content-item/${id}`);
   const data = await res.json();
   return {
-    props: { rony: data },
-  };
+    props: { ronys: data }
+  }
 }
-const Detail = ({ rony }) => {
+const Details = ({ ronys }) => {
   return (
     <>
-      <div className="container-fluid">
+    
+<div className="container-fluid">
         <Head>
-          <title>{rony.title}</title>
-          <meta name="description" content="Ifad Autos Tesimonial" />
+          <title>News</title>
+          <meta name="description" content="Ifad Autos News" />
           <link rel="icon" href="/favicon.ico" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <Top />
         <Homeslider />
-        <main className="container">
+        {ronys.map(item=>{
+          return(
+        <main className="container" key={item.id}>
           <div className="row my-5">
             <h1 className="brandColor fw-bold text-center">News & Events</h1>
           </div>
           <div className="row my-5">
             <div className="col-sm-8">
-              <h1>{rony.title}</h1>
+              <h1>{item.item_name}</h1>
               <div className="col-sm-12 position-relative">
                 <Image
                   className="position-relative"
-                  layout="fill"
-                  objectFit="contain"
-                  src={rony.image}
-                  alt=""
+                  width={850}
+                  height={500}
+                  src={item.item_image}
+                  alt="news"
                 />
               </div>
-              <p className="my-3">{rony.description}</p>
+              <p className="my-3">{item.item_long_desc}</p>
             </div>
             <div className="col-sm-4">
               <div>
@@ -69,10 +76,14 @@ const Detail = ({ rony }) => {
           </div>
           <h4 className="brandColor fw-bold">Related Articles</h4>
         </main>
+          )
+        })}
       </div>
-      <Relatednews />
+    
+      
+      {/* <Relatednews /> */}
       <Footer />
     </>
   );
 };
-export default Detail;
+export default Details;

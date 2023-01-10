f4f95522cae3dccc4c5e66a9adfb81e2830d4397
rony1980 <rauf.rony@gmail.com>
2023-01-10 21:16:30 +0600

    Update Recentnews.jsx

--- pages/news/Recentnews.jsx
+++ pages/news/Recentnews.jsx
@@ -4,31 +4,35 @@ import Image from "next/image";
 const Recentnews = () => {
   const [news, setNews] = useState([]);
   useEffect(() => {
-    fetch("https://fakestoreapi.com/products/")
+    fetch("http://autosapi.ifadgroup.com:8001/content-module/17")
       .then((res) => res.json())
       .then((data) => setNews(data));
   }, []);
   return (
     <div>
       <h4 className="brandColor fw-bold">Recent Posts</h4>
-      {news.slice(0, 5).map((ron) => (
-        <div className="row border-bottom py-4" key={ron.id}>
+      {news.map((ron) => 
+        ron.content_item.map(item=>{
+        return(
+        <div className="row border-bottom py-4" key={item.id}>
           <div className="col-3 position-relative">
             <Image
               className="position-relative"
               layout="fill"
               objectFit="cover"
-              src={ron.image}
+              src={item.item_image}
               alt="image"
             />
           </div>
           <div className="col-9 fs-3">
-            <Link className="newslink" key={ron.id} href={`${ron.id}`}>
-              <h4>{ron.title}</h4>
+            <Link className="newslink" key={item.id} href={`${item.id}`}>
+              <h4>{item.item_name}</h4>
             </Link>
           </div>
         </div>
-      ))}
+        )
+         })
+        )}
     </div>
   );
 };

5a9227bdfedcf19b4bf5845354283bad6f4b0ba7
rony1980 <rauf.rony@gmail.com>
2023-01-10 21:10:46 +0600

    Update index.js

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,22 +1,28 @@
 import Link from "next/link";
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://fakestoreapi.com/products/");
+  const res = await fetch(
+    "http://autosapi.ifadgroup.com:8001/content-module/17"
+  );
   const data = await res.json();
   return {
     props: { rony: data },
   };
 };
-const Products = ({ rony }) => {
+const News = ({ rony }) => {
   return (
     <div>
-      {rony.map((ron) => (
-        <Link key={ron.id} href={`Product/${ron.id}`}>
-          <h4>{ron.title}</h4>
-        </Link>
-      ))}
+      {rony.map((ron) =>
+        ron.content_item.map((item) => {
+          return (
+            <Link key={item.id} href={`news/${item.id}`}>
+              <h4>{item.item_name}</h4>
+            </Link>
+          );
+        })
+      )}
     </div>
   );
 };
 
-export default Products;
+export default News;
